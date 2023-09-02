6798bbe64ed0dde6e45358f139c75e88e7ac14c6
Kramzaid <lesterlaurente0329@yahoo.com>
2023-09-02 10:26:28 +0800

    added a new file browser-support.txt

--- Hello.java
+++ Hello.java
@@ -1,7 +1,8 @@
 public class Hello {
   public static void main (String[] args)
   {
-    System.out.println("Hello, Software Engineering Course!")
-    System.out.println("My name is LAURENTE, Mark Lester!")    
+	System.out.println("Hello, Software Engineering Course!");
+  	System.out.println("My name is LAURENTE, Mark Lester!");
+	System.out.println("That is why developers are highly-paid!");
   }
 }
